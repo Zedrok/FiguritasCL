4c08ef43820755d00cfa14d83eab468a89309995
ShindouDoushin <jose.silva.a@mail.pucv.cl>
2020-09-26 20:31:34 -0300

    Screen cuenta responsiva

--- src/app/services/user/usuario.service.ts
+++ src/app/services/user/usuario.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private usuario:Usuario;
 
-  constructor() { }
+  constructor() { 
+    this.usuario.nombre ="Lol";
+    this.usuario.numero ="Lol";
+    this.usuario.rut ="Lol";
+    this.usuario.apellidoP ="Lol";
+    this.usuario.apellidoM ="Lol";
+    this.usuario.direccion ="Lol";
+    this.usuario.correo ="Lol";
+    //this.usuario.fechaDeNacimiento =date.actual;
+  }
 }
