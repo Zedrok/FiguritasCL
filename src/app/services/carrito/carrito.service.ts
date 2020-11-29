2fd4636e6780bf935dae33c983203c7a870c1e76
Hernan <hernanastudillom@gmail.com>
2020-11-29 16:18:34 -0300

    todo correcto

---
+++ src/app/services/carrito/carrito.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { PRODUCT } from '../product/PRODUCT.const';
+import { ProductoCarrito } from '../../models/productoCarrito.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarritoService {
+
+  productos = PRODUCT;
+  items: ProductoCarrito[] = [];
+  encontrado = false;
+  constructor() {
+  }
+
+  agregarCarrito(producto): void{
+    let encontrado = false;
+    this.items.forEach((elemento) => {
+    if (elemento._id === producto._id){
+        encontrado = true;
+        elemento.cantidad += 1;
+      }
+    });
+
+    if (!this.encontrado) {
+      this.items.push({ _id : producto._id, nombre : producto.nombre, precio : producto.precio, cantidad : 1});
+    }
+  }
+
+  limpiarCarrito(): any[]{
+    this.items = [];
+    return this.items;
+  }
+
+  ListarCarrito(): any[]{
+    return this.items;
+  }
+}
